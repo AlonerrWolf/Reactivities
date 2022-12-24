ff00f6e0eac4486555521bf7c14e04bb4619178b
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-12-24 17:17:19 +0300

    Add project files.

---
+++ Reactivities.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Reactivities.API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        //api/activities/1
+        [HttpGet("{id}")] //api/activities
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+
+    }
+}
