6b1a899ac418057684ca54dd56aac4fa7bea121c
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2023-01-02 09:21:41 +0300

    Routing Added

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,6 +2,7 @@ import React, {SyntheticEvent, useState} from 'react';
 import {Button, Item, Label, Segment} from "semantic-ui-react";
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
+import {Link} from "react-router-dom";
 
 
 export default observer(function ActivityList() {
@@ -29,8 +30,7 @@ export default observer(function ActivityList() {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button floated='right' content='View' color='blue'
-                                        onClick={() => activityStore.selectActivity(activity.id)}/>
+                                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue'/>
                                 <Button name={activity.id} floated='right' content='Delete'
                                         loading={loading && target === activity.id}
                                         color='red'
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,42 +1,66 @@
-import React, { ChangeEvent, useState } from 'react'
-import { Button, Form, Segment } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity';
+import React, {ChangeEvent, useEffect, useState} from 'react'
+import {Button, Form, Segment} from 'semantic-ui-react'
+import {Activity} from '../../../app/models/activity';
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
+import {Link, useNavigate, useParams} from "react-router-dom";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {v4 as uuid} from 'uuid';
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {selectedActivity,closeForm,createActivity,updateActivity,loading} = activityStore
-    const initialState = selectedActivity ?? {
-        id:'',
+    const navigate = useNavigate();
+    const {
+         createActivity, updateActivity, loading,
+        loadActivity, loadingInitial
+    } = activityStore
+    const {id} = useParams();
+
+    const [activity, setActivity] = useState<Activity>({
+        id: '',
         title: '',
         description: '',
         category: '',
         date: '',
         city: '',
         venue: '',
-    }
-    const[activity,setActivity] = useState(initialState);
+    });
+
+    useEffect(()=>{
+        if (id) loadActivity(id).then(activity=>setActivity(activity!))
+    },[id,loadActivity])
+
     function handleSubmit() {
+        if (!activity.id){
+            activity.id = uuid();
+            createActivity(activity).then(()=>navigate(`/activities/${activity.id}`))
+        } else{
+            updateActivity(activity).then(()=>navigate(`/activities/${activity.id}`))
+        }
         activity.id ? updateActivity(activity) : createActivity(activity);
     }
+
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const {name, value} = event.target;
-        setActivity({...activity,[name]: value})
+        setActivity({...activity, [name]: value})
     }
+    if (loadingInitial) return <LoadingComponent content='Loading activity...'/>
 
-    return(
+    return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange}/>
-                <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}/>
-                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
-                <Form.Input type='date' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
+                <Form.TextArea placeholder='Description' value={activity.description} name='description'
+                               onChange={handleInputChange}/>
+                <Form.Input placeholder='Category' value={activity.category} name='category'
+                            onChange={handleInputChange}/>
+                <Form.Input type='date' placeholder='Date' value={activity.date} name='date'
+                            onChange={handleInputChange}/>
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
                 <Button loading={loading} floated='right' positive type='submit' content='Submit'/>
-                <Button onClick={closeForm} floated='right' positive type='button' content='Cancel'/>
-                
+                <Button as={Link} to='/activities' floated='right' positive type='button' content='Cancel'/>
+
             </Form>
         </Segment>
     )
