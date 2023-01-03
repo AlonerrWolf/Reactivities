3c19f59f2eaed4a685727b38815a3fbb38da3f11
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2023-01-03 14:16:55 +0300

    Server-side error handling

---
+++ Application/Core/AppException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public class AppException
+    {
+        public AppException(string message, int statusCode, string? details = null)
+        {
+            Message = message;
+            Details = details;
+            StatusCode = statusCode;
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ Reactivities.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using Application.Core;
+
+namespace Reactivities.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<Exception> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<Exception> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new AppException(ex.Message, context.Response.StatusCode, ex.StackTrace)
+                    : new AppException("Internal Server Error", context.Response.StatusCode);
+
+                var options = new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+
+            }
+        }
+
+    }
+}
