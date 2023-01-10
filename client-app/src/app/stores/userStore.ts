b2cc496e24e84c8838c27e95c014f5e2f375bbef
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2023-01-10 17:51:43 +0300

    Client Image Upload

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,8 +1,9 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { User, UserFormValues } from "../models/user";
-import { router} from "../../router/Routes";
+import { router } from "../../router/Routes";
 import { store } from "./store";
+
 export default class UserStore {
     user: User | null = null;
 
@@ -52,5 +53,13 @@ export default class UserStore {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+    }
+
+    setUserPhoto = (url: string) => {
+        if (this.user) this.user.image = url;
     }
 }
